d0bd63aff6a4f00fd4f47025f2c8ac53609be445
PaulineF <flambard.pauline@laposte.net>
2016-02-23 17:09:07 +0100

    Recherche dans une liste

--- js/pokedex.js
+++ js/pokedex.js
@@ -5,3 +5,17 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 }]);
 
 var pokeApiUrl = "http://pokeapi.co/"
+
+var app = angular.module("pokedex", []);
+
+app.controller("pokedexCtrl", function($scope, $log) {
+    $scope.$log = $log;
+        $scope.pokemons = [
+            {value: 0, displayName: 'Pikatchu'},
+            {value: 1, displayName: 'Salameche'},
+            {value: 2, displayName: 'Rondoudou'},
+            {value: 3, displayName: 'Carapuce'},
+            {value: 4, displayName: 'Bulbizarre'},
+        ]
+
+});
