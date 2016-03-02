6439f5e4e7f9894b42c6cdaaf256f0d6f9c7fb66
PaulineF <flambard.pauline@laposte.net>
2016-03-02 17:30:33 +0100

    service

--- js/pokedex.js
+++ js/pokedex.js
@@ -5,42 +5,35 @@ pokeApp.config(['$resourceProvider', function($resourceProvider) {
 }]);
 
 var pokeApiUrl = "http://pokeapi.co/"
-var pokemon = {name: "t"};
+
+
 var app = angular.module("pokedex", ['ngResource']);
 
-app.controller("pokedexCtrl", function($scope, $log, $http, $resource) {
+app.factory('searchPokemon', function($resource){
+    return $resource("http://pokeapi.co/api/v2/pokemon-species/:id/", {id:'@id'});
+
+});
+app.controller("pokedexCtrl", function($scope, $log, $http, $resource, searchPokemon) {
     $scope.$log = $log;
-       /* $scope.pokemons = [
-            {value: 0, displayName: 'Pikatchu'},
-            {value: 1, displayName: 'Salameche'},
-            {value: 2, displayName: 'Rondoudou'},
-            {value: 3, displayName: 'Carapuce'},
-            {value: 4, displayName: 'Bulbizarre'},
-        ];
-*/
+
     $http({
         method: 'GET',
-        url: 'http://pokeapi.co/api/v1/pokedex/1'
+        url: 'http://pokeapi.co/api/v2/pokedex/1'
     }).then(function successCallback(response) {
         //console.log(response.data)
-        $scope.pokemons = response.data.pokemon;
+        $scope.pokemons = response.data.pokemon_entries;
     });
+    $scope.go = function(idPoke){
 
-    $scope.go = function(url){
-        console.log(url);
-        var test = $resource("http://pokeapi.co/"+url)
-        pokemon = test.get({}, function() {
 
-            //user.abc = true;
-            pokemon.$save();
-        });
-        console.log(pokemon);
+        $scope.pokemonObj = searchPokemon.get({id:idPoke});
+
     }
-
-
-
 })
 
 app.controller("affichage", function($scope, $resource){
-    $scope.pokemonname = pokemon.name;
+
 });
+/*
+
+        console.log(pokemon);*/
